fbb0a78e898d476c63c34cf0098f17e1b294d8f5
arrshsh <mad31122002vahora@gmail.com>
2025-05-27 00:58:32 -0400

    setup file added

---
+++ setup.py
@@ -0,0 +1,15 @@
+# this is done to make the local package, so that we can import stuffs from src folder 
+# for example, if we wanna do :
+# from src.helper import main
+# it'll throw an error until we dont make a local package of src
+
+from setuptools import find_packages, setup
+
+setup(
+    name = "End-to-end Medical Chatbot",
+    version = "0.0.0",
+    author= "Mohammad Arsh Vahora",
+    author_email= "vahora4@uwindsor.ca",
+    packages= find_packages(), # this looks for the constructor file i.e. __init__.py in each folder and wherever it finds that, that folder is made into a package
+    install_requires = []
+)
